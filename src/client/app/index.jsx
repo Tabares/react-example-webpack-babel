b66d448a40fb72658e25e8d6b601b0adc2d283c3
Tabares Sotelo, Jose <jose.tabares.sotelo@accenture.com>
2016-09-05 13:29:09 -0500

    Added component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
+import AwesomeComponent from './Component.jsx';
+
 
 class App extends React.Component {
   render () {
-    return <p><h1> Master started kit with React, webpack and babel!</h1><br/><p>Npm as tool runner</p></p>;
+    return (
+      <div>
+        <h1> Master started kit with React, webpack and babel!</h1>
+        <br/><p>Npm as tool runner</p>
+        <br/><h1> This is a Component!</h1>
+        <AwesomeComponent/>
+      </div>
+    );
   }
 }
 
--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -57,6 +57,10 @@
 	
 	var _reactDom = __webpack_require__(/*! react-dom */ 34);
 	
+	var _Component = __webpack_require__(/*! ./Component.jsx */ 173);
+	
+	var _Component2 = _interopRequireDefault(_Component);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -78,7 +82,7 @@
 	    key: 'render',
 	    value: function render() {
 	      return _react2.default.createElement(
-	        'p',
+	        'div',
 	        null,
 	        _react2.default.createElement(
 	          'h1',
@@ -90,7 +94,14 @@
 	          'p',
 	          null,
 	          'Npm as tool runner'
-	        )
+	        ),
+	        _react2.default.createElement('br', null),
+	        _react2.default.createElement(
+	          'h1',
+	          null,
+	          ' This is a Component!'
+	        ),
+	        _react2.default.createElement(_Component2.default, null)
 	      );
 	    }
 	  }]);
@@ -21983,6 +21994,83 @@
 	
 	module.exports = ReactDOMNullInputValuePropHook;
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(/*! ./~/process/browser.js */ 3)))
+
+/***/ },
+/* 172 */,
+/* 173 */
+/*!**************************************!*\
+  !*** ./src/client/app/Component.jsx ***!
+  \**************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var AwesomeComponent = function (_React$Component) {
+	  _inherits(AwesomeComponent, _React$Component);
+	
+	  function AwesomeComponent(props) {
+	    _classCallCheck(this, AwesomeComponent);
+	
+	    var _this = _possibleConstructorReturn(this, (AwesomeComponent.__proto__ || Object.getPrototypeOf(AwesomeComponent)).call(this, props));
+	
+	    _this.state = { PushesCount: 0 };
+	    _this.onLike = _this.onLike.bind(_this);
+	    return _this;
+	  }
+	
+	  _createClass(AwesomeComponent, [{
+	    key: 'onLike',
+	    value: function onLike() {
+	      var newLikesCount = this.state.PushesCount + 1;
+	      this.setState({ PushesCount: newLikesCount });
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        'Pushes : ',
+	        _react2.default.createElement(
+	          'span',
+	          null,
+	          this.state.PushesCount
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          null,
+	          _react2.default.createElement(
+	            'button',
+	            { onClick: this.onLike },
+	            'Push Me'
+	          )
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return AwesomeComponent;
+	}(_react2.default.Component);
+	
+	exports.default = AwesomeComponent;
 
 /***/ }
 /******/ ]);
