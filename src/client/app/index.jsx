10485e4fe11c8605fa7ae523e65e6c2e3bd4773e
Tabares Sotelo, Jose <jose.tabares.sotelo@accenture.com>
2016-09-05 13:03:34 -0500

    Initialized React also setup npm as tool runner

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,2 +1,10 @@
-console.log('Hello World!');
-console.log('Hello World with webpack!');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <p><h1> Master started kit with React, webpack and babel!</h1><br/><p>Npm as tool runner</p></p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
